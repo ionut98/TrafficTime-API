00726842ae48b032f4053f1b12633d5df3adc392
ionutc3 <ionutmihoci@yahoo.com>
2020-10-01 13:40:41 +0300

    [FIX] SOLVED MONGO DB CONNECTION

---
+++ db/index.js
@@ -0,0 +1,28 @@
+const {
+  MongoClient,
+} = require('mongodb');
+
+
+const {
+  uri,
+} = require('./dbConfig');
+
+const client = new MongoClient(uri, { useUnifiedTopology: true });
+
+const connectToDB = async () => {
+
+  try {
+    await client.connect();
+
+    const result = await client.db().admin().listDatabases();
+    console.log(result);
+    
+  } catch (error) {
+    console.log(error);
+  } finally {
+    await client.close();
+  }
+
+};
+
+connectToDB();
